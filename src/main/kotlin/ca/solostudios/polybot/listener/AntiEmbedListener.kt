a4c12d870a22b5cc362f73a384ed6a7af24bade4
solonovamax <solonovamax@12oclockpoint.com>
2021-12-30 17:28:58 -0500

    Add embed suppression for all domains, which include "github"
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

---
+++ src/main/kotlin/ca/solostudios/polybot/listener/AntiEmbedListener.kt
@@ -0,0 +1,67 @@
+/*
+ * PolyhedralBot - A Discord bot for the Polyhedral Development discord server
+ * Copyright (c) 2021-2021 solonovamax <solonovamax@12oclockpoint.com>
+ *
+ * The file AntiEmbedListener.kt is part of PolyhedralBot
+ * Last modified on 30-12-2021 05:28 p.m.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * POLYHEDRALBOT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package ca.solostudios.polybot.listener
+
+import ca.solostudios.polybot.Constants
+import ca.solostudios.polybot.PolyBot
+import dev.minn.jda.ktx.await
+import java.net.URL
+import kotlinx.coroutines.launch
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import org.slf4j.kotlin.*
+
+class AntiEmbedListener(val bot: PolyBot) : ListenerAdapter() {
+    private val logger by getLogger()
+    
+    override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
+        bot.scope.launch {
+            val message = event.message
+            val messageContentRaw = message.contentRaw
+            
+            if (message.embeds.isNotEmpty()) {
+                val suppress = Constants.urlRegex.findAll(messageContentRaw)
+                        .any {
+                            runCatching {
+                                val url = URL(it.value)
+                                url.host.contains("github")
+                            }.getOrElse { false }
+                        }
+                
+                logger.info { "suppress=$suppress" }
+                
+                if (suppress) {
+                    logger.debug { "Should suppress embed for message ${message.idLong}." }
+                    message.suppressEmbeds(true)
+                            .await()
+                }
+            }
+        }
+    }
+}
